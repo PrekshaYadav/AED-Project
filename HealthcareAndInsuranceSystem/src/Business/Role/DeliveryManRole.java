5eec0d83f73f658844d3ea8d91f9f95ade1c1027
PrekshaYadav <113851464+PrekshaYadav@users.noreply.github.com>
2022-12-06 19:41:07 -0500

    HC and Pharmacy Roles

---
+++ HealthcareAndInsuranceSystem/src/Business/Role/DeliveryManRole.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.DB4OUtil.DB4OUtil;
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.DeliveryManOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.DeliveryManRole.DeliveryManWorkAreaJPanel;
+
+/**
+ *
+ * @author Sumit
+ */
+public class DeliveryManRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount userAccount, Organization organization, Enterprise enterprise, EcoSystem business,DB4OUtil dB4OUtil) {
+        return new DeliveryManWorkAreaJPanel(userProcessContainer, userAccount, (DeliveryManOrganization) organization, enterprise, dB4OUtil, business);
+    }
+
+}
